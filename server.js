ea00830783322fd5bea42c5c63d14a1eeb17c75d
Willy Wario <willywario0@gmail.com>
2022-06-24 16:30:42 +0300

    ready for heroku

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
+//Install express server
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname +"/dist"));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
 
-app.all('*', (req, res) =>{
-    res.status(200).sendFile(__dirname + "/dist/index.html");
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist//'}),
+);
 
-app.listen(process.env.PORT || 8080);   
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);  
